dc715855c0f989d9c62e9b36586b6158178c6528
rawan ayman <ronyayman68@gmail.com>
2026-05-16 15:13:34 +0300

    Initial Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Shop from "./components/Shop";
+import Ingredients from "./components/Ingredients";
+import About from "./components/About";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen overflow-hidden bg-gradient-to-br from-[#FFF8F2] via-[#FDF2F8] to-[#EEF2FF]">
+      <Navbar />
+      <Hero />
+      <Shop />
+      <Ingredients />
+      <About />
+      <Reviews />
+      <Contact />
     </div>
   );
 }
